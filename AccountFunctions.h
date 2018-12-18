2aa92dfe5bf784248f46bad515f1bb92f7622ba4
Phil2k19 <45975113+Phil2k19@users.noreply.github.com>
2018-12-18 11:43:36 -0500

    Create AccountFunctions.h

---
+++ AccountFunctions.h
@@ -0,0 +1,3 @@
+double DepositFunc(double Deposit, double &CurrentBalance);
+double WithdrawalFunc(double Withdrawal, double &CurrentBalance);
+double InterestRate(double &CurrentBalance, double AIR, double MonthlyBalance);
